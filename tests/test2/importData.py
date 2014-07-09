39ff8d65c4db545d9a986696b1baba82c0a9b8b6
mlujan <mike.lujan@gmail.com>
2014-07-08 23:39:43 -0400

    Clean up

--- tests/test2/importData.py
+++
@@ -1,9 +0,0 @@
-
-import os
-from sst.actions import *
-from sst import runtests
-
-#Load Our custom Functions
-execfile("_functions/importCsv.py")
-path = os.path.dirname(os.path.abspath(__file__))
-data = loadmain(path +"/_data/testdata.csv")
